64525ef9e306fe330da0249330c3f99eccf9a88d
Brett Longworth <blongworth@gmail.com>
2017-01-03 09:20:01 -0500

    remove automatic file gen

--- firms.sh
+++ firms.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Script to convert IRMS output files to db input format.
-
-if [ "${1:0:1}"  == "P" ]
+# Will pick correct system based on first char of file
+# Still need correct column for Optima data
+a=$(basename $1)
+if [ "${a:0:1}"  == "P" ]
 then
 	csvcut -H -c 6,15 $1 |\
-	sed -e '/INST3 GAS/d' -e '1d' -e 's/$/,Prism/' | csvformat -T \
-	> final$1
+	sed -e '/INST3 GAS/d' -e '1d' -e 's/$/,Prism/' | csvformat -T
 else
 	csvcut -c 1,49 $1 |\
-	sed -e '/INST3 GAS/d' -e '1d' -e 's/$/,Optima/' | csvformat -T \
-	> final$1
+	sed -e '/INST3 GAS/d' -e '1d' -e 's/$/,Optima/' | csvformat -T
 fi
